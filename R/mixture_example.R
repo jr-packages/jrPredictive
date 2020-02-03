f77e9f12f7cb037889e8877fd11b9717ebc5196b
Jamie <jamie@jumpingrivers.com>
2020-02-03 13:30:10 +0000

    add in mixture data from now archived elemstatlearn

---
+++ R/mixture_example.R
@@ -0,0 +1,8 @@
+#' The mixture example data set
+#'
+#' Mixture example data from the now archived ElemStatLearn package
+#' @name mixture.example
+#' @docType data
+#' @examples
+#' data(mixture.example)
+NULL
